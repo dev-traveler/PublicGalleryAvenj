84aa8ac41e929a6bb62287f79b6b5886194dd966
dev-traveler <avenj.dev@gmail.com>
2022-01-23 22:51:59 +0900

    Make focus move when enter key press at input

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,49 +1,123 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import BorderedInput from '../components/BorderedInput';
 import CustomButton from '../components/CustomButton';
 
 function SignInScreen({navigation, route}) {
-  const {isSignUp} = route.params ?? {};
+  const {isSignUp} = route.params || {};
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const createChangeTextHandler = name => value => {
+    setForm({...form, [name]: value});
+  };
+  const onSubmit = () => {
+    Keyboard.dismiss();
+    console.log(form);
+  };
+
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
   return (
-    <SafeAreaView style={styles.fullscreen}>
-      <Text style={styles.text}>PublicGallery</Text>
-      <View style={styles.form}>
-        <BorderedInput placeholder="이메일" hasMarginBottom />
-        <BorderedInput placeholder="비밀번호" hasMarginBottom={isSignUp} />
-        {isSignUp && <BorderedInput placeholder="비밀번호 확인" />}
-        <View style={styles.buttons}>
-          {isSignUp ? (
-            <>
-              <CustomButton title="회원가입" hasMarginBottom />
-              <CustomButton
-                title="로그인"
-                theme="secondary"
-                onPress={() => {
-                  navigation.goBack();
-                }}
-              />
-            </>
-          ) : (
-            <>
-              <CustomButton title="로그인" hasMarginBottom />
-              <CustomButton
-                title="회원가입"
-                theme="secondary"
-                onPress={() => {
-                  navigation.push('SignIn', {isSignUp: true});
-                }}
-              />
-            </>
+    <KeyboardAvoidingView
+      style={styles.keyboardAvoidingView}
+      behavior={Platform.select({ios: 'padding'})}>
+      <SafeAreaView style={styles.fullscreen}>
+        <Text style={styles.text}>PublicGallery</Text>
+        <View style={styles.form}>
+          <BorderedInput
+            hasMarginBottom
+            placeholder="이메일"
+            value={form.email}
+            onChangeText={createChangeTextHandler('email')}
+            autoCapitalize="none"
+            autoCorrect={false}
+            autoCompleteType="email"
+            keyboardType="email-address"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+          <BorderedInput
+            placeholder="비밀번호"
+            secureTextEntry
+            hasMarginBottom={isSignUp}
+            value={form.password}
+            onChangeText={createChangeTextHandler('password')}
+            ref={passwordRef}
+            returnKeyType={isSignUp ? 'next' : 'done'}
+            onSubmitEditing={() => {
+              if (isSignUp) {
+                confirmPasswordRef.current.focus();
+              } else {
+                onSubmit();
+              }
+            }}
+          />
+          {isSignUp && (
+            <BorderedInput
+              placeholder="비밀번호 확인"
+              secureTextEntry
+              value={form.confirmPassword}
+              onChangeText={createChangeTextHandler('confirmPassword')}
+              ref={confirmPasswordRef}
+              returnKeyType="done"
+              onSubmitEditing={onSubmit}
+            />
           )}
+          <View style={styles.buttons}>
+            {isSignUp ? (
+              <>
+                <CustomButton
+                  title="회원가입"
+                  hasMarginBottom
+                  onPress={onSubmit}
+                />
+                <CustomButton
+                  title="로그인"
+                  theme="secondary"
+                  onPress={() => {
+                    navigation.goBack();
+                  }}
+                />
+              </>
+            ) : (
+              <>
+                <CustomButton
+                  title="로그인"
+                  hasMarginBottom
+                  onPress={onSubmit}
+                />
+                <CustomButton
+                  title="회원가입"
+                  theme="secondary"
+                  onPress={() => {
+                    navigation.push('SignIn', {isSignUp: true});
+                  }}
+                />
+              </>
+            )}
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
+  keyboardAvoidingView: {
+    flex: 1,
+  },
   fullscreen: {
     flex: 1,
     alignItems: 'center',
