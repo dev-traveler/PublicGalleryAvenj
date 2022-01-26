ed63ba50a550e62968d010d5d15045bc81c95c6a
dev-traveler <avenj.dev@gmail.com>
2022-01-26 23:38:38 +0900

    Add SetupProfile for creating user using firestore

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -9,11 +9,14 @@ import {
   View,
 } from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {useNavigation} from '@react-navigation/native';
 import SignButtons from '../components/SignButtons';
 import SignInForm from '../components/SignForm';
 import {signIn, signUp} from '../lib/auth';
+import {getUser} from '../lib/users';
 
 function SignInScreen({route}) {
+  const navigation = useNavigation();
   const {isSignUp} = route.params || {};
   const [form, setForm] = useState({
     email: '',
@@ -28,18 +31,26 @@ function SignInScreen({route}) {
 
   const onSubmit = async () => {
     Keyboard.dismiss();
+
     const {email, password, confirmPassword} = form;
 
     if (isSignUp && password !== confirmPassword) {
       Alert.alert('실패', '비밀번호가 일치하지 않습니다.');
+      console.log({password, confirmPassword});
       return;
     }
 
     setLoading(true);
     const info = {email, password};
+
     try {
       const {user} = isSignUp ? await signUp(info) : await signIn(info);
-      console.log(user);
+      const profile = await getUser(user.uid);
+      if (!profile) {
+        navigation.navigate('Welcome', {uid: user.uid});
+      } else {
+        // 구현 예정
+      }
     } catch (e) {
       const messages = {
         'auth/email-already-in-use': '이미 가입된 이메일입니다.',
@@ -47,7 +58,7 @@ function SignInScreen({route}) {
         'auth/user-not-found': '존재하지 않는 계정입니다.',
         'auth/invalid-email': '유효하지 않은 이메일 주소입니다.',
       };
-      const msg = messages[e.code] || `${isSignUp ? '가입' : '로그인'} 실패 `;
+      const msg = messages[e.code] || `${isSignUp ? '가입' : '로그인'} 실패`;
       Alert.alert('실패', msg);
     } finally {
       setLoading(false);
